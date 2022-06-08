f8a83ef66162bba6ff273eac02ae153ac2b4f934
Henry <henryhein.hein@gmail.com>
2022-06-08 03:57:36 -0700

    NFT Collection Tutorial

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,29 @@
+const { ethers } = require('hardhat');
+require('dotenv').config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  console.log("Crypto Devs Contract Address", deployedCryptoDevsContract.address);
+
+}
+
+main()
+  .then( () => process.exit(0))
+  .catch( (error) => {
+    console.error(error);
+    process.exit(1);
+  });
